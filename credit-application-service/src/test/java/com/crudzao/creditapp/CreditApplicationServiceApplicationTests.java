a24f4393b66453c7ab693d5876c24d1e8bed8269
JuanCamilo <Juancamilosanchezmendez@gmail.com>
2025-12-09 15:15:56 -0500

    Initial commit: credit-application-service and risk-central-mock-service

---
+++ credit-application-service/src/test/java/com/crudzao/creditapp/CreditApplicationServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.crudzao.creditapp;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CreditApplicationServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
